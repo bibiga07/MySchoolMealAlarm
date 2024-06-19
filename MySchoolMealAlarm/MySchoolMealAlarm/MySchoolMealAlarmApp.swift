649b5a8c865963601cca7cd7ae8edb9bf10e66fc
bibiga07 <130065479+bibiga07@users.noreply.github.com>
2024-06-19 11:06:23 +0900

    🎉 Start Project

---
+++ MySchoolMealAlarm/MySchoolMealAlarm/MySchoolMealAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySchoolMealAlarmApp.swift
+//  MySchoolMealAlarm
+//
+//  Created by bibiga on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct MySchoolMealAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
